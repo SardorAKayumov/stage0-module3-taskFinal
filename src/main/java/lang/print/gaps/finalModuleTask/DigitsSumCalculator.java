6649d96a373a52ddbef996fdabf774ecf36fb741
SardorAKayumov <sardor.zeroz@gmail.com>
2022-09-26 15:51:16 +0500

    done!

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int answer = 0;
+        while(number > 0) {
+            answer +=  number % 10;
+            number /= 10;
+        }
+        System.out.println(answer);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int answer = 0;
+        while(number > 0) {
+            answer = answer * 10 + number % 10;
+            number /= 10;
+        }
+        System.out.println(answer);
     }
 }
